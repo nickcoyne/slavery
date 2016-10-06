11a0f511b19a5dba5d39474e8853fd2087b7475f
Nick <nick@codevader.com>
2016-10-07 11:28:14 +1300

    Prefer alias_method
    Using module prepend as implemented caused stack too deep errors when
    performing counts.

--- lib/slavery/relation.rb
+++ lib/slavery/relation.rb
@@ -1,25 +1,23 @@
-module WithSlavery
+class ActiveRecord::Relation
   attr_accessor :slavery_target
 
   # Supports queries like User.on_slave.to_a
+  alias_method :original_exec_queries, :exec_queries
   def exec_queries
     if slavery_target == :slave
-      Slavery.on_slave { super }
+      Slavery.on_slave { original_exec_queries }
     else
-      super
+      original_exec_queries
     end
   end
 
   # Supports queries like User.on_slave.count
+  alias_method :original_calculate, :calculate
   def calculate(operation, column_name)
     if slavery_target == :slave
-      Slavery.on_slave { super(operation, column_name) }
+      Slavery.on_slave { original_calculate(operation, column_name) }
     else
-      super(operation, column_name)
+      original_calculate(operation, column_name)
     end
   end
-end
-
-class ActiveRecord::Relation
-  prepend WithSlavery
 end
